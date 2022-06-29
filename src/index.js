d1a861555f6e68d48d6c16e994491ccf339a4bfc
Larissa Moreira <larissamoreiracarvalho@gmail.com>
2022-06-29 14:13:31 -0300

    tentativa filtro

--- src/index.js
+++ src/index.js
@@ -3,36 +3,35 @@ import financeiro from "./routes/financeiro/index.js";
 import educacional from "./routes/educacional/index.js";
 import servicos from "./routes/servicos/index.js";
 
-
 const main = document.querySelector("#root");
 
 const init = () => {
 
-    main.innerHTML="";
+  main.innerHTML = "";
 
-    switch(window.location.hash){
-      case "#":
-        main.appendChild(gestaoPessoal());
-        break;
-      case "#financeiro":
-        main.appendChild(financeiro());
-        break;
-      case "#educacional":
-        main.appendChild(educacional());
-        break;
-      case "#servicos":
-        main.appendChild(servicos());
-        break;
-      default:
-        main.appendChild(gestaoPessoal());
-    }
+  switch (window.location.hash) {
+    case "#":
+      main.appendChild(gestaoPessoal());
+      break;
+    case "#financeiro":
+      main.appendChild(financeiro());
+      break;
+    case "#educacional":
+      main.appendChild(educacional());
+      break;
+    case "#servicos":
+      main.appendChild(servicos());
+      break;
+    default:
+      main.appendChild(gestaoPessoal());
+  }
 }
 
-const redirect = () =>{
-window.addEventListener("hashchange", () => {
-  main.innerHTML = "";
-  init();
-});
+const redirect = () => {
+  window.addEventListener("hashchange", () => {
+    main.innerHTML = "";
+    init();
+  });
 };
 
 window.addEventListener("load", () => {
@@ -40,3 +39,25 @@ window.addEventListener("load", () => {
   redirect();
 })
 
+var nomeCard = document.getElementById('searchType');
+
+function searchCards() {
+  let find = nomeCard.value
+  console.log(find);
+
+  find = find.toLowerCase();
+  let classCard = document.getElementsByClassName('sectionCard');
+  console.log(classCard);
+
+  for (let i = 0; i < classCard.length; i++) {
+    if (!classCard[i].innerHTML.toLowerCase().includes(find)) {
+      classCard[i].style.display = "none";
+    }
+    else {
+      classCard[i].style.display = "list-item";
+    }
+  }
+
+}
+
+nomeCard.addEventListener("keyup", searchCards);
