fb08b26f1e47c6bfc6774c8f2402cd5bca3d4ddf
Larissa Moreira <larissamoreiracarvalho@gmail.com>
2022-06-30 11:15:18 -0300

    Updates

--- routes/servicos/index.js
+++ routes/servicos/index.js
@@ -2,18 +2,18 @@ export default () => {
     const container = document.createElement("div");
     container.classList.add('containerMain');
 
-  const template = `
+    const template = `
   <section class="sectionCard cardMagenta">
-      <img class="iconCard" src="../../image/reserva-de-salas.svg"/>
+      <img class="iconCard" src="./image/reserva-de-salas.svg"/>
       <button class="btncard">Reserva de Salas</button>
   </section>
   <section class="sectionCard cardRoxo">
-      <img class="iconCard" src="../../image/video-conferencia.svg"/>
-      <button class="btncard">Audio Conferências</button>
+      <img class="iconCard" src="./image/video-conferencia.svg"/>
+      <button class="btncard">Áudio Conferências</button>
   </section>
   <section class="sectionCard cardAzul">
-      <img class="iconCard" src="../../image/central-atendimento.svg"/>
-      <button class="btncard">Service Desk</button>
+      <img class="iconCard" src="./image/central-atendimento.svg"/>
+      <button class="btncard">Central de Ajuda</button>
   </section>
   `;
 
