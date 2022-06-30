92646988f246fd90b7e6f59652d1f7cd5a663fba
Larissa Moreira <larissamoreiracarvalho@gmail.com>
2022-06-30 11:12:34 -0300

    acentuação 100%

--- src/routes/gestaoPessoal/index.js
+++ src/routes/gestaoPessoal/index.js
@@ -2,26 +2,26 @@ export default () => {
     const container = document.createElement("div");
     container.classList.add('containerMain');
 
-  const template = `
+    const template = `
   <section class="sectionCard cardMagenta">
-      <img class="iconCard" src="../../image/controle-de-ponto.svg"/>
+      <img class="iconCard" src="./image/controle-de-ponto.svg"/>
       <button class="btncard">Controle de Ponto</button>
   </section>
   <section class="sectionCard cardRoxo">
-      <img class="iconCard" src="../../image/fluxo-de-trabalho.svg"/>
+      <img class="iconCard" src="./image/fluxo-de-trabalho.svg"/>
       <button class="btncard">Estrutura Organizacional</button>
   </section>
   <section class="sectionCard cardAzul">
-      <img class="iconCard" src="../../image/holerite.svg"/>
+      <img class="iconCard" src="./image/holerite.svg"/>
       <button class="btncard">Holerite</button>
   </section>
   <section class="sectionCard cardMagenta">
-      <img class="iconCard" src="../../image/ferias.svg"/>
-      <button class="btncard">Ferias</button>
+      <img class="iconCard" src="./image/ferias.svg"/>
+      <button class="btncard">Férias</button>
   </section>
   <section class="sectionCard cardRoxo">
-      <img class="iconCard" src="../../image/desligamento.svg"/>
-      <button class="btncard">Portal de desligamento</button>
+      <img class="iconCard" src="./image/desligamento.svg"/>
+      <button class="btncard">Portal de Desligamento</button>
   </section>
   `;
 
